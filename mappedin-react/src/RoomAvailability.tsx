766d5b4f9f3397a45b0678ff57a480e850ad2dcf
Ekaums <ekaum.singh@gmail.com>
2024-09-14 17:36:19 -0400

    dropdown for rooms

--- mappedin-react/src/RoomAvailability.tsx
+++ mappedin-react/src/RoomAvailability.tsx
@@ -1,27 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import data from '../data/data.json'
 
-export default function RoomAvailability(){
+export default function RoomSelector() {
+
+    // Initalize state. Do I need to init with default here?
+    const [room, setRoom] = useState("");
+
     return (
-        <div>
-          <h1>Classroom Availability</h1>
-    
-          {/* Loop through the rooms array */}
-          {data.rooms.map((room, index) => (
-            <div key={index}>
-              {/* Name of each room */}
-              <h2>{room.name}</h2>
-    
-              {/* Availability dates for each room */}
-              <ul>
-                {room.dates.map((dateInfo, idx) => (
-                  <li key={idx}>
-                    {dateInfo.date}: {dateInfo.start} - {dateInfo.end}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
+        <div
+        style={{
+            position: "absolute",
+            bottom: "10px",
+            right: "10px",
+            zIndex: 1000,
+            display: "flex",
+            alignItems: "center",
+            background: "rgba(255, 255, 255, 0.8)",
+            padding: "10px",
+            borderRadius: "5px",
+            boxShadow: "0px 0px 5px rgba(0, 0, 0, 0.2)",
+          }}
+          className="text-3xl p-10"
+          > 
+            <select 
+                defaultValue={data.rooms[0]["name"]}
+                onChange={(e) => {
+                    // const room = data.rooms.find((r) => r.name === roomName) // triple = vs double = ?
+                    setRoom(e.target.value);
+                }}
+            >
+            {data.rooms.map((room, idx) => (
+                <option key={idx} value={room.name}>
+                {room.name}
+                </option>
+            ))}
+            </select>
         </div>
-      );
+    )
 }
