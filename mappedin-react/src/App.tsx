e1ad911a15d3943ed56497a4f1204fd800dce430
Siddharth <88167499+SiddharthN16@users.noreply.github.com>
2024-09-14 19:56:55 -0400

    State management & working room selector (#5)

--- mappedin-react/src/App.tsx
+++ mappedin-react/src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { MapView, useMapData } from "@mappedin/react-sdk";
 import "@mappedin/react-sdk/lib/esm/index.css";
 import FloorSelector from "./FloorSelector";
@@ -11,9 +10,9 @@ export default function App() {
   // See Demo API key Terms and Conditions
   // https://developer.mappedin.com/v6/demo-keys-and-maps/
   const { isLoading, error, mapData } = useMapData({
-    key: 'mik_jLcoTTRYFT3O05Su152e306f6',
-    secret: 'mis_0rSTYuJi6ivxlsbRb8P157jwriG1xVU8IocaK0YldqI94881083',
-    mapId: '66e50265af770b000b90802d'
+    key: "mik_jLcoTTRYFT3O05Su152e306f6",
+    secret: "mis_0rSTYuJi6ivxlsbRb8P157jwriG1xVU8IocaK0YldqI94881083",
+    mapId: "66e50265af770b000b90802d",
   });
 
   if (isLoading) {
